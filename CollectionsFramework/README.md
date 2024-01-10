7621e586510d1a2f0d210764b2cb81439b151f86
Dhanushka sandakelum <62172201+DhanushkaSandakelum@users.noreply.github.com>
2024-01-10 22:59:30 +0530

    Concurrency first example added

---
+++ CollectionsFramework/README.md
@@ -0,0 +1,5 @@
+## Collections Framework
+
+<div style="background-color: white; padding: 20px;">
+<img src="./collections_framwork_ref.png" />>
+</div>
---
+++ Concurrency/src/Task.java
@@ -0,0 +1,16 @@
+public class Task implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Inside run");
+        go();
+    }
+
+    private void go() {
+        System.out.println("Inside go");
+        more();
+    }
+
+    private void more() {
+        System.out.println("Inside more");
+    }
+}
